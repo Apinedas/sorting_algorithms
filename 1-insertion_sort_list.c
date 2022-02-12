a176d7364a0a26247be11120b0704b2b382e6166
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-02-12 03:19:04 +0000

    [UPDATE] Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -31,25 +31,24 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current_node, *previous_node, *next_node;
 
-	if (list && *list)
+	if (!list || !(*list))
+		return;
+	current_node = *list;
+	while (current_node)
 	{
-		current_node = *list;
-		while (current_node)
+		previous_node = current_node->prev;
+		next_node = current_node->next;
+		while (previous_node)
 		{
-			previous_node = current_node->prev;
-			next_node = current_node->next;
-			while (previous_node)
+			if (current_node->n < previous_node->n)
 			{
-				if (current_node->n < previous_node->n)
-				{
-					swap_nodes(list, previous_node, current_node);
-					print_list(*list);
-					previous_node = current_node->prev;
-				}
-				else
-					break;
+				swap_nodes(list, previous_node, current_node);
+				print_list(*list);
+				previous_node = current_node->prev;
 			}
-			current_node = next_node;
+			else
+				break;
 		}
+		current_node = next_node;
 	}
 }
