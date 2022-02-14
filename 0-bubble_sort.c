bb42badf338bbe3f12d5a36a8a454b3e2b143d73
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-02-14 01:41:43 +0000

    [UPDATE] Corrections on selection sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,19 +12,18 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int aux;
 
-	if (array && size > 0)
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < (size - 1); i++)
 	{
-		for (i = 0; i < (size - 1); i++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
-			for (j = 0; j < (size - i - 1); j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					aux = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = aux;
-					print_array(array, size);
-				}
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
 			}
 		}
 	}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,25 +9,30 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int smaller, smaller_index, aux;
+	size_t i, j, smaller_index;
+	int smaller, aux, change;
 
-	if (!array || size <= 0)
+	if (!array || size < 2)
 		return;
 	for (i = 0; i <= (size - 1); i++)
 	{
 		smaller = array[i];
+		change = 0;
 		for (j = i + 1; j <= (size - 1); j++)
 		{
 			if (array[j] < smaller)
 			{
 				smaller = array[j];
 				smaller_index = j;
+				change += 1;
 			}
 		}
-		aux = array[i];
-		array[i] = smaller;
-		array[smaller_index] = aux;
-		print_array(array, size);
+		if (i != smaller_index && change > 0)
+		{
+			aux = array[i];
+			array[i] = smaller;
+			array[smaller_index] = aux;
+			print_array(array, size);
+		}
 	}
 }
